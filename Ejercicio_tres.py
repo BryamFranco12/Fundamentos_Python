fa46cb769ccbe9d3a5e784206c8c8677cdbb12d0
BryamFranco12 <142601252+BryamFranco12@users.noreply.github.com>
2023-08-22 21:47:40 -0500

    Delete Ejercicio_tres.py

--- Ejercicio_tres.py
+++
@@ -1,5 +0,0 @@
-N1 = int(input("Primer numero: "))
-N2 = int(input("Segundo numero: "))
-N3 = int(input("Tercer numero: "))
-
-print("El numero mayor entre [{}]-[{}]-[{}] es el {}".format(N1, N2, N3, max(N1, N2, N3)))
